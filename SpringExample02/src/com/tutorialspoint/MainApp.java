4642d492bd5d8d160871510a7e8ca2477b35a95a
favio-torrico <favio.torrico@gmail.com>
2017-05-15 17:24:55 +0200

    Primera actualizacion Spring2

---
+++ SpringExample02/src/com/tutorialspoint/MainApp.java
@@ -0,0 +1,19 @@
+package com.tutorialspoint;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+   public static void main(String[] args) {
+      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");// enlaza con Beans XML
+      
+      HelloWorld objA = (HelloWorld) context.getBean("helloWorld");// coge el bean helloWorld y lo castea
+      objA.getMessage1();
+      objA.getMessage2();
+
+      HelloIndia objB = (HelloIndia) context.getBean("helloIndia");// coge el bean helloIndia y lo castea
+      objB.getMessage1();
+      objB.getMessage2();
+      objB.getMessage3();
+   }
+}
